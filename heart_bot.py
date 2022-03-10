1daa23c4ad03952dd85c874d6641e8ab2987d1f5
roctbb <roctbb@gmail.com>
2022-03-10 18:07:33 +0300

    Update heart_bot.py

--- heart_bot.py
+++ heart_bot.py
@@ -9,7 +9,7 @@ from agents_api import *
 from mail_api import *
 
 app = Flask(__name__)
-db_string = "postgres://{}:{}@{}:{}/{}".format(DB_LOGIN, DB_PASSWORD, DB_HOST, DB_PORT, DB_DATABASE)
+db_string = "postgresql://{}:{}@{}:{}/{}".format(DB_LOGIN, DB_PASSWORD, DB_HOST, DB_PORT, DB_DATABASE)
 app.config['SQLALCHEMY_DATABASE_URI'] = db_string
 db = SQLAlchemy(app)
 
