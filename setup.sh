aacf52fa024f0da0c083580d16f91eb43cff8710
roctbb <roctbb@gmail.com>
2023-02-06 22:15:43 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,4 +3,4 @@ sudo cp agents_heart.conf /etc/supervisor/conf.d/
 sudo cp agents_heart_nginx.conf /etc/nginx/sites-enabled/
 sudo supervisorctl update
 sudo systemctl restart nginx
-sudo certbot --nginx -d heart.medsenger.ru
+sudo certbot --nginx -d heart.ai.medsenger.ru
